1b1b6f904b12ace94516d1ed497c3552db8be9dd
amerzone-exe <jocelyn.piquet1998@gmail.com>
2026-09-21 17:25:51 +0200

    corrected ex00

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: jpiquet <jpiquet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 17:05:56 by jpiquet           #+#    #+#             */
-/*   Updated: 2026/09/16 15:54:42 by jpiquet          ###   ########.fr       */
+/*   Updated: 2026/09/21 17:13:21 by jpiquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -136,10 +136,10 @@ float	BitcoinExchange::getExchangeValue( std::string const& date, float value)
 {
 	std::map<std::string, float>::iterator it = _dataCsv.lower_bound(date);
 
+	if (it == _dataCsv.begin())
+		return value * it->second;
 	if (it == _dataCsv.end() || it->first != date)
 		it--;
-	if (it == _dataCsv.begin())
-		return 0;
 	return value * it->second;
 }
 
